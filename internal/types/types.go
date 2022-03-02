0ea4209408e3c51080b3d034c989d84b1f74f53b
David Haukeness <david@hauken.us>
2022-03-02 10:29:09 -0700

    change to []byte and render markdown

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,8 +1,18 @@
 package types
 
+import "github.com/charmbracelet/glamour"
+
 type MenuEntry struct {
 	Name  string
-	Value string
+	Value []byte
+}
+
+func (m *MenuEntry) RenderBytes() (string, error) {
+	b, err := glamour.RenderBytes(m.Value, "dark")
+	if err != nil {
+		return string(m.Value), err
+	}
+	return string(b), nil
 }
 
 type SelectedMessage struct {
